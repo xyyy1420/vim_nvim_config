07b4ce32fc05f00a5746dd85f9f0c97d8bd68471
jxy <xyyy1420@gmail.com>
2023-02-20 20:09:34 +0800

    feat:新增treesitter的部分设置
    Signed-off-by: jxy <xyyy1420@gmail.com>

--- init/nvim_plug_config/treesitter.lua
+++ init/nvim_plug_config/treesitter.lua
@@ -1,21 +1,22 @@
-require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all"
-      ensure_installed = { "c", "go" ,"lua", "rust","cpp","python" },
+local ts_utils = require('nvim-treesitter.ts_utils')
+require 'nvim-treesitter.configs'.setup {
+    -- A list of parser names, or "all"
+    ensure_installed = { "c", "go", "lua", "rust", "cpp", "python", "kdl" },
 
-      -- Install parsers synchronously (only applied to `ensure_installed`)
-      sync_install = true,
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = true,
 
-      -- Automatically install missing parsers when entering buffer
-      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-      auto_install = true,
+    -- Automatically install missing parsers when entering buffer
+    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+    auto_install = true,
 
-      -- List of parsers to ignore installing (for "all")
-      --    ignore_install = { "javascript" },
+    -- List of parsers to ignore installing (for "all")
+    --    ignore_install = { "javascript" },
 
-      ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-      -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+    ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+    -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
-      highlight = {
+    highlight = {
         -- `false` will disable the whole extension
         enable = true,
 
@@ -23,7 +24,7 @@ require'nvim-treesitter.configs'.setup {
         -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
         -- the name of the parser)
         -- list of language that will be disabled
-        disable = {  },
+        disable = {},
         -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
         --disable = function(lang, buf)
         --    local max_filesize = 100 * 1024 -- 100 KB
@@ -38,5 +39,10 @@ require'nvim-treesitter.configs'.setup {
         -- Using this option may slow down your editor, and you may see some duplicate highlights.
         -- Instead of true it can also be a list of languages
         additional_vim_regex_highlighting = false,
-      },
+
+    },
+    indent = {
+        enable = true
+    },
+    textobjects = { enable = true },
 }
